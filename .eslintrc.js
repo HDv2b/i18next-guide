636baeac60d2c9b666f5512eeafa2ee88711175c
Hussein Duvigneau <hussein@notmybase.com>
2020-10-12 03:20:39 +0200

    interpolation improvements

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     'react',
   ],
   rules: {
-    // suppress errors for missing 'import React' in files
     'react/react-in-jsx-scope': 'off',
+    'react/jsx-one-expression-per-line': 'off',
   },
 };
--- pages/interpolation.jsx
+++ pages/interpolation.jsx
@@ -10,19 +10,28 @@ const Homepage = ({ t }) => {
     <main>
       <Header title="Interpolation Example" />
       <div className="preview">
-        <label htmlFor="name">Name: </label>
-        <input
-          id="name"
-          onChange={(e) => setField({ ...fields, name: e.target.value })}
-          defaultValue={fields.name}
-        />
-        <br />
-        <label htmlFor="location">Location: </label>
-        <input
-          id="location"
-          onChange={(e) => setField({ ...fields, location: e.target.value })}
-          defaultValue={fields.location}
-        />
+        <dl>
+          <dt>
+            <label htmlFor="name">{t('interpolation:name')}: </label>
+          </dt>
+          <dd>
+            <input
+              id="name"
+              onChange={(e) => setField({ ...fields, name: e.target.value })}
+              defaultValue={fields.name}
+            />
+          </dd>
+          <dt>
+            <label htmlFor="location">{t('interpolation:location')}: </label>
+          </dt>
+          <dd>
+            <input
+              id="location"
+              onChange={(e) => setField({ ...fields, location: e.target.value })}
+              defaultValue={fields.location}
+            />
+          </dd>
+        </dl>
         <br />
         <br />
         <p>{t('interpolation:aboutPerson', fields)}</p>
