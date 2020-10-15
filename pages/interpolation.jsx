86793cf0ca29ce62be93eee724775c2c339d17c1
Hussein Duvigneau <hussein@notmybase.com>
2020-10-16 01:41:49 +0200

    extract example to own components, and add tips

--- pages/interpolation.jsx
+++ pages/interpolation.jsx
@@ -3,42 +3,51 @@ import { useState } from 'react';
 import { withTranslation } from '../i18n';
 import Header from '../components/Header';
 
-const Homepage = ({ t }) => {
+const InterpolationDemo = ({ t }) => {
   const [fields, setField] = useState({ name: 'Michael', location: 'London' });
-
   return (
-    <main>
-      <Header title="Interpolation Example" />
-      <div className="preview">
-        <dl>
-          <dt>
-            <label htmlFor="name">{t('interpolation:name')}: </label>
-          </dt>
-          <dd>
-            <input
-              id="name"
-              onChange={(e) => setField({ ...fields, name: e.target.value })}
-              defaultValue={fields.name}
-            />
-          </dd>
-          <dt>
-            <label htmlFor="location">{t('interpolation:location')}: </label>
-          </dt>
-          <dd>
-            <input
-              id="location"
-              onChange={(e) => setField({ ...fields, location: e.target.value })}
-              defaultValue={fields.location}
-            />
-          </dd>
-        </dl>
-        <br />
-        <br />
-        <p>{t('interpolation:aboutPerson', fields)}</p>
-      </div>
-    </main>
+    <div>
+      <dl>
+        <dt>
+          <label htmlFor="name">{t('interpolation:name')}: </label>
+        </dt>
+        <dd>
+          <input
+            id="name"
+            onChange={(e) => setField({ ...fields, name: e.target.value })}
+            defaultValue={fields.name}
+          />
+        </dd>
+        <dt>
+          <label htmlFor="location">{t('interpolation:location')}: </label>
+        </dt>
+        <dd>
+          <input
+            id="location"
+            onChange={(e) => setField({ ...fields, location: e.target.value })}
+            defaultValue={fields.location}
+          />
+        </dd>
+      </dl>
+      <p>{t('interpolation:aboutPerson', fields)}</p>
+    </div>
   );
 };
+
+const Homepage = ({ t }) => (
+  <main>
+    <Header title="Interpolation Example" />
+    <div className="preview">
+      <InterpolationDemo t={t} />
+    </div>
+    <div>
+      <h2>Tips</h2>
+      <ul>
+        <li>In the case of dynamic text, it's not enough to simply translate and swap word for word, because different languages will order their words differently. For example, switch to German and watch the verb take the place place instead of the city name.</li>
+      </ul>
+    </div>
+  </main>
+);
 
 Homepage.getInitialProps = async () => ({
   namespacesRequired: ['common', 'footer', 'interpolation'],
--- pages/plurals.jsx
+++ pages/plurals.jsx
@@ -3,33 +3,45 @@ import { useState } from 'react';
 import { withTranslation } from '../i18n';
 import Header from '../components/Header';
 
-const Homepage = ({ t }) => {
+const PluralsDemo = ({ t }) => {
   const [number, setNumber] = useState(1);
 
   return (
-    <main>
-      <Header title="Plurals example" />
-      <div>
-        <div className="preview">
-          <div>
-            <p>
-              Change the counter below and watch the sentence update to use the appropriate&nbsp;
-              <a href="https://en.wikipedia.org/wiki/Grammatical_number">grammatical number</a>
-              .
-            </p>
-            <input type="number" onChange={(e) => setNumber(parseInt(e.target.value, 10))} defaultValue={number} />
-            <p>{t('plurals:keyWithCount', { count: number })}</p>
-          </div>
-        </div>
-
-        <p>
-          see:
-          <a href="https://jsfiddle.net/sm9wgLze">https://jsfiddle.net/sm9wgLze</a>
-        </p>
-      </div>
-    </main>
+    <div>
+      <input
+        type="number"
+        onChange={(e) => setNumber(parseInt(e.target.value, 10))}
+        defaultValue={number}
+      />
+      <p>{t('plurals:keyWithCount', { count: number })}</p>
+    </div>
   );
 };
+
+const Homepage = ({ t }) => (
+  <main>
+    <Header title="Plurals example" />
+    <div>
+      <div className="preview">
+        <p>
+          Change the counter below and watch the sentence update to use the appropriate&nbsp;
+          <a href="https://en.wikipedia.org/wiki/Grammatical_number">grammatical number</a>
+          .
+        </p>
+        <PluralsDemo t={t} />
+      </div>
+      <div>
+        <h2>Tips</h2>
+        <ul>
+          <li>Remember that even in English, it's not helpful to just stick an "s" at the end of every word to make it plural. For example "sheep" and "hooves" and "wellies".</li>
+          <li>In some languages, the adjective is also pluralized.</li>
+          <li>In English we only have singular and plural, but other languages have different "<a href="https://en.wikipedia.org/wiki/Grammatical_number">grammatical numbers</a>". For example some language feature "Dual", where the word is spelt differently than either its singular or plural form. This is just one example of different grammatical numbers out there. For another example, switch to Russian (ru), and notice the spelling change between 0, 1, 2, and 5 items.</li>
+          <li>For a guide on which languages' grammatical numbers are supported, see <a href="https://jsfiddle.net/sm9wgLze">https://jsfiddle.net/sm9wgLze</a></li>
+        </ul>
+      </div>
+    </div>
+  </main>
+);
 
 Homepage.getInitialProps = async () => ({
   namespacesRequired: ['common', 'footer', 'plurals'],
