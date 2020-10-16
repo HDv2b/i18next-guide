2acf81bb768d11ff401ed537c7716a7521ae69fb
Hussein Duvigneau <hussein@notmybase.com>
2020-10-16 14:27:41 +0200

    styling

--- pages/plurals.jsx
+++ pages/plurals.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { withTranslation } from '../i18n';
+import { i18n, withTranslation } from '../i18n';
 import Header from '../components/Header';
 import PluralsExample from '../components/Examples/Plurals';
 
@@ -8,37 +8,51 @@ const Plurals = ({ exampleSrc, i18nFiles }) => (
   <main>
     <Header title="Plurals example" />
     <div>
-      <div className="preview">
+      <section className="preview">
         <p>
           Change the counter below and watch the sentence update to use the appropriate&nbsp;
           <a href="https://en.wikipedia.org/wiki/Grammatical_number">grammatical number</a>
           .
         </p>
         <PluralsExample />
-      </div>
-      <div>
-        <SyntaxHighlighter language="jsx" showLineNumbers>
-          {exampleSrc}
-        </SyntaxHighlighter>
-        <SyntaxHighlighter language="json">
-          {i18nFiles.en}
-        </SyntaxHighlighter>
-        <SyntaxHighlighter language="json">
-          {i18nFiles.de}
-        </SyntaxHighlighter>
-        <SyntaxHighlighter language="json">
-          {i18nFiles.ru}
-        </SyntaxHighlighter>
-      </div>
-      <div>
+      </section>
+      <section>
+        <h2>Code</h2>
+        <div>
+          <SyntaxHighlighter language="jsx" showLineNumbers>
+            {exampleSrc}
+          </SyntaxHighlighter>
+        </div>
+        <div className="i18n-snippet-container" data-selected-lang={i18n.language}>
+          <div className="i18n-snippet" data-lang="en">
+            <p>plurals/en.json</p>
+            <SyntaxHighlighter language="json">
+              {i18nFiles.en}
+            </SyntaxHighlighter>
+          </div>
+          <div className="i18n-snippet" data-lang="de">
+            <p>plurals/de.json</p>
+            <SyntaxHighlighter language="json">
+              {i18nFiles.de}
+            </SyntaxHighlighter>
+          </div>
+          <div className="i18n-snippet" data-lang="ru">
+            <p>plurals/ru.json</p>
+            <SyntaxHighlighter language="json">
+              {i18nFiles.ru}
+            </SyntaxHighlighter>
+          </div>
+        </div>
+      </section>
+      <section className="tips">
         <h2>Tips</h2>
         <ul>
           <li>Remember that even in English, it's not helpful to just stick an "s" at the end of every word to make it plural. For example "sheep" and "hooves" and "wellies".</li>
           <li>In some languages, the adjective is also pluralized.</li>
           <li>In English we only have singular and plural, but other languages have different "<a href="https://en.wikipedia.org/wiki/Grammatical_number">grammatical numbers</a>". For example some language feature "Dual", where the word is spelt differently than either its singular or plural form. This is just one example of different grammatical numbers out there. For another example, switch to Russian (ru), and notice the spelling change between 0, 1, 2, and 5 items.</li>
-          <li>For a guide on which languages' grammatical numbers are supported, see <a href="https://jsfiddle.net/sm9wgLze">https://jsfiddle.net/sm9wgLze</a></li>
+          <li>For a guide for how to support individual languages' grammatical numbers, see <a href="https://jsfiddle.net/sm9wgLze">https://jsfiddle.net/sm9wgLze</a>.</li>
         </ul>
-      </div>
+      </section>
     </div>
   </main>
 );
