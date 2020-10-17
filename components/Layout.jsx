b643183f319dbb679104a64255147b99241d5911
Hussein Duvigneau <hussein@notmybase.com>
2020-10-17 17:10:06 +0200

    remove favicon

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -12,7 +12,6 @@ const Layout = ({ children }) => (
 
       <link href="https://cdnjs.cloudflare.com/ajax/libs/typicons/2.0.9/typicons.min.css" rel="stylesheet" />
       <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400|Oswald:600" rel="stylesheet" />
-      <link data-react-helmet="true" rel="icon" href="https://blobscdn.gitbook.com/v0/b/gitbook-28427.appspot.com/o/spaces%2F-L9iS6Wm2hynS5H9Gj7j%2Favatar.png?generation=1523462254548780&amp;alt=media" />
     </Head>
     <div>
       <header><h1>i18next Guide (WIP)</h1></header>
